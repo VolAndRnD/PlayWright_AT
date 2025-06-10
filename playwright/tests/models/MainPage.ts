4c6af9ecd8b62f0e6a680caa2c3173b2460d859f
unknown <volk0096@mail.ru>
2025-06-10 11:11:56 +0300

    add models mainpage

---
+++ playwright/tests/models/MainPage.ts
@@ -0,0 +1,72 @@
+import { Page } from '@playwright/test';
+import { test, expect } from '@playwright/test';
+
+export class MainPage {
+  readonly page: Page;
+
+  constructor(page: Page) {
+    this.page = page;
+  }
+
+  async openMainPage() {
+    await this.page.goto('https://release.workhere.ru/');
+    await this.page.waitForLoadState('load');
+    await this.page.getByTestId('login').click();
+    await this.page.getByRole('switch', { name: 'Я соискатель Я работодатель' }).click();
+    await expect(this.page.locator("//span[@class='switch-text-color__Ca2S1']")).toContainText(
+      'Я соискатель',
+      { timeout: 3000 },
+    );
+  }
+
+  async enterPhoneNumber() {
+    await test.step('Смена номера телефона под код страны Беларусь', async () => {
+      const countryButton = await this.page.getByRole('button', { name: 'Russia: +' });
+      await expect(countryButton).toBeVisible({ timeout: 3000 });
+      await countryButton.click();
+      await expect(this.page.getByRole('option', { name: 'Belarus+' })).toBeVisible({
+        timeout: 3000,
+      });
+      const belarusOption = await this.page.getByRole('option', { name: 'Belarus+' });
+      await belarusOption.click();
+    });
+    await test.step('Ввод номера телефона пользователя', async () => {
+      const activeField = await this.page.getByRole('textbox', { name: '+375 (29) 999-99-' });
+      await expect(activeField).toBeVisible();
+      await activeField.click();
+      await activeField.fill('+375 (30) 000 00 02');
+    });
+  }
+
+  async enterSMSPassword() {
+    await test.step('Ввод кода авторизации из смс', async () => {
+      const buttonEnter = await this.page.getByRole('button', { name: 'Войти' });
+      await expect(buttonEnter).toBeVisible();
+      await buttonEnter.click();
+
+      const activeTextBox = await this.page.getByRole('textbox', { name: '* Код подтверждения' });
+      await expect(activeTextBox).toBeVisible();
+      await activeTextBox.click();
+      await activeTextBox.fill('1111');
+    });
+  }
+
+  async enterLoginPassword() {
+    await test.step('Активация поля для ввода пароля', async () => {
+      const buttonUsePassword = await this.page.getByText('Войти, используя пароль');
+      await expect(buttonUsePassword).toBeVisible();
+      await buttonUsePassword.click();
+    });
+    await test.step('Ввод пароля', async () => {
+      const PasswordField = await this.page.getByRole('textbox', { name: '* Пароль' });
+      await expect(PasswordField).toBeVisible();
+      await PasswordField.click();
+      await PasswordField.fill('gbhjvfy');
+    });
+    await test.step('вход в ЛК', async () => {
+      const buttonEnter = await this.page.getByRole('button', { name: 'Войти' });
+      await expect(buttonEnter).toBeVisible();
+      await buttonEnter.click();
+    });
+  }
+}
