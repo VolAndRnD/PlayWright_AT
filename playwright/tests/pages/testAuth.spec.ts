4392b861dad297a52686145baa31ce271a8015d5
unknown <volk0096@mail.ru>
2025-06-18 18:07:01 +0300

    fix

--- playwright/tests/pages/testAuth.spec.ts
+++ playwright/tests/pages/testAuth.spec.ts
@@ -3,30 +3,34 @@ import { authElements, authUIElements } from '../locators/AuthLocators';
 import { lendAuthElements, lendUIElements } from '../locators/LendLocators';
 import { LendingPage } from '../models/LendingPage';
 
-test.describe('Тесты способов авторизации работодателя', () => {
-  test(`Авторизация c помощью  со страницы лендинга`, async ({ lendingPage, browser }) => {
+test.describe('Тесты авторизации работодателя', () => {
+  test(`Авторизация со страницы лендинга`, async ({ lendingPage, browser }) => {
     test.setTimeout(60000);
-
-    for (const testCase of lendAuthElements) {
-      const context = await browser.newContext();
-      const page = await context.newPage();
-      const lendingPage = new LendingPage(page);
-      try {
-        await lendingPage.openMainPage();
-        await lendingPage.openAuthModal(lendUIElements);
-        await lendingPage.enterPhoneNumber(testCase);
-        if (testCase.name === 'SMS') {
-          await lendingPage.enterSMSPassword(testCase);
-        } else {
-          await lendingPage.enterLoginPassword(testCase);
+    try {
+      for (const testCase of lendAuthElements) {
+        const context = await browser.newContext();
+        const page = await context.newPage();
+        const lendingPage = new LendingPage(page);
+        try {
+          await lendingPage.openMainPage();
+          await lendingPage.openAuthModal(lendUIElements);
+          await lendingPage.enterPhoneNumber(testCase);
+          if (testCase.name === 'SMS') {
+            await lendingPage.enterSMSPassword(testCase);
+          } else {
+            await lendingPage.enterLoginPassword(testCase);
+          }
+        } finally {
+          await context.close();
         }
-      } finally {
-        await context.close();
       }
+    } catch (error) {
+      console.error('Ошибка в тесте:', error);
+      throw error;
     }
   });
 
-  test('Авторизация со страницы авторизации site-login', async ({ authPage }) => {
+  test('Авторизация со страницы ЛК site-login', async ({ authPage }) => {
     test.setTimeout(60000);
     try {
       await authPage.openAuthPage();
